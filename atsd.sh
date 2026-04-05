09518e8c9df73fb91fe968538d03ea3a23416316
hmf <manfu_huang@163.com>
2026-04-05 11:00:10 +0800

    train all dataset with SWAN

---
+++ atsd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+CMD="sh ./scripts/covariate_forecasting/SWAN_quick.sh"  # 这里填入你启动模型训练时的命令，比如这里我用python run.py指令启动模型
+ 
+ # 判断当前指令是不是在跑
+if ! pgrep -f "$CMD" > /dev/null; then
+    echo "错误：程序 $CMD 没有在运行，退出脚本。"
+    exit 1
+else
+    echo "程序开始运行了"
+fi
+
+
+while pgrep -f "$CMD" > /dev/null; do
+    sleep 60  # 每隔1min检查一次
+done
+ 
+echo "程序已结束，正在关机..."
+/usr/bin/shutdown -h now
