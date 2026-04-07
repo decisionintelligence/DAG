18b91a91170eb65d6c189d6fa895be1957af7d1f
hmf <manfu_huang@163.com>
2026-04-07 10:42:52 +0800

    swan_v3 train all

---
+++ atsd_mul.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+CMD1="sh ./scripts/covariate_forecasting/SWAN_V3_ALL_part1.sh"
+CMD2="sh ./scripts/covariate_forecasting/SWAN_V3_ALL_part2.sh"
+
+if ! pgrep -f "$CMD1" > /dev/null; then
+    echo "错误：程序 $CMD1 没有在运行，退出脚本。"
+    exit 1
+fi
+
+if ! pgrep -f "$CMD2" > /dev/null; then
+    echo "错误：程序 $CMD2 没有在运行，退出脚本。"
+    exit 1
+fi
+
+echo "所有程序开始运行了"
+
+while pgrep -f "$CMD1" > /dev/null || pgrep -f "$CMD2" > /dev/null; do
+    sleep 60
+done
+
+echo "所有程序已结束，正在关机..."
+/usr/bin/shutdown -h now
